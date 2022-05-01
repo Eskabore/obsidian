25da419ac39b800a426ffd174f5bb574bd4b4c5c
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2022-05-01 21:42:12 +0200

    CS%)0- 01052022, Scrabble, ongoing

---
+++ PKM/MyDesktop/CS50x/Weekly Materials/Week 0/Submitted Material/scrabble.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+
+int compute_score(string word) 
+{
+    int scrore = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        // Convert to lowercase
+        word[i] = tolower(word[i]);
+        // Check if the letter is a letter
+        if (isaplhabet(word[i]))
+        {
+            // Add the points to the score
+            score += points[word[i] - 'a'];
+        }
+    }
+}
