33e0487e7c61112a153b37f0ebde907d3e0dba80
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2022-06-30 10:41:06 +0200

    Update30.6

--- PKM/MyDesktop/FreeCodeCamp/FrontEndDevelopmentLibrairies/Projects/C-Random-Quote-Machine/random-quote-machine-fcc-2/src/components/QuoteMachine.js
+++ PKM/MyDesktop/FreeCodeCamp/FrontEndDevelopmentLibrairies/Projects/C-Random-Quote-Machine/random-quote-machine-fcc-2/src/components/QuoteMachine.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const QuoteMachine = (props) => (
   <Card>
@@ -18,7 +21,10 @@ const QuoteMachine = (props) => (
     }
     </CardContent>
     <CardActions>
-    <Button size="small" onClick={props.assignNewQuoteIndex} >NEW QUOTE</Button>
+    <Button size="small" onClick={props.assignNewQuoteIndex}>NEW QUOTE</Button>
+    <IconButton>
+        <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+    </IconButton>
     </CardActions>
   </Card>
 );
